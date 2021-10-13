13d40873cffecf527a0828567d83ea7f838aa50f
taiga-tech <taiga.dev.n@gmail.com>
2021-10-14 03:41:18 +0900

    add Other Section

---
+++ src/components/Home/Sections/Social.js
@@ -0,0 +1,12 @@
+// mui components
+import Container from '@mui/material/Container'
+// local constants
+// import { SOCIAL } from 'constants/social'
+
+export const SocialSection = () => {
+  return (
+    <Container>
+      <h1>Social</h1>
+    </Container>
+  )
+}
---
+++ src/components/Home/Sections/Video.js
@@ -0,0 +1,10 @@
+// mui components
+import Container from '@mui/material/Container'
+
+export const VideoSection = () => {
+  return (
+    <Container>
+      <h1>Video</h1>
+    </Container>
+  )
+}
