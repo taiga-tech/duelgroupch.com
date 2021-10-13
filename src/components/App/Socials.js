1fa1a8ace862841a456e171fc3f9368f449f0384
taiga-tech <taiga.dev.n@gmail.com>
2021-10-14 03:37:42 +0900

    Update Socials.js

--- src/components/App/Socials.js
+++ src/components/App/Socials.js
@@ -1,82 +1,26 @@
 // mui components
 import Stack from '@mui/material/Stack'
 import IconButton from '@mui/material/IconButton'
-// mui icons
-import YouTubeIcon from '@mui/icons-material/YouTube'
-import InstagramIcon from '@mui/icons-material/Instagram'
-import TwitterIcon from '@mui/icons-material/Twitter'
-import GitHubIcon from '@mui/icons-material/GitHub'
-import MusicNoteSharpIcon from '@mui/icons-material/MusicNote'
+// local constants
+import { SOCIALS } from 'constants/social'
 
 export const Socials = () => {
   return (
     <Stack spacing={2} direction="row">
-      <IconButton
-        sx={{
-          color: '#ff0100',
-          '&:hover': { backgroundColor: 'rgba(255 , 1, 0, 0.04)' },
-        }}
-        target="_blank"
-        rel="noopener noreferrer"
-        href="https://www.youtube.com/channel/UCE010VqCfjLp7zckSBbFyfw/"
-        aria-label="youtube"
-        title="YouTube"
-        size="small"
-      >
-        <YouTubeIcon />
-      </IconButton>
-      <IconButton
-        sx={{
-          color: '#1d9bef',
-          '&:hover': { backgroundColor: 'rgba(29 , 155, 239, 0.04)' },
-        }}
-        target="_blank"
-        rel="noopener noreferrer"
-        href="https://twitter.com/DuelGroup/"
-        aria-label="twitter"
-        title="Twitter"
-        size="small"
-      >
-        <TwitterIcon />
-      </IconButton>
-      <IconButton
-        sx={{
-          color: '#CF2E92',
-          '&:hover': { backgroundColor: 'rgba(207 , 45, 146, 0.04)' },
-        }}
-        target="_blank"
-        rel="noopener noreferrer"
-        href="https://www.instagram.com/duelgroup/"
-        aria-label="instagram"
-        title="Instagram"
-        size="small"
-      >
-        <InstagramIcon />
-      </IconButton>
-      <IconButton
-        target="_blank"
-        rel="noopener noreferrer"
-        href="https://vt.tiktok.com/ZSJTKdV4D/"
-        aria-label="tiktok"
-        title="TikTok"
-        size="small"
-      >
-        <MusicNoteSharpIcon />
-      </IconButton>
-      <IconButton
-        sx={{
-          color: 'purple',
-          '&:hover': { backgroundColor: 'rgba(128,0,128,0.04)' },
-        }}
-        target="_blank"
-        rel="noopener noreferrer"
-        href="https://github.com/taiga-tech/"
-        aria-label="github"
-        title="GitHub"
-        size="small"
-      >
-        <GitHubIcon />
-      </IconButton>
+      {SOCIALS.map((sns, i) => (
+        <IconButton
+          sx={sns.sx}
+          target="_blank"
+          rel="noopener noreferrer"
+          href={sns.href}
+          aria-label={sns.title}
+          title={sns.title}
+          size="small"
+          key={i}
+        >
+          {sns.icon}
+        </IconButton>
+      ))}
     </Stack>
   )
 }
