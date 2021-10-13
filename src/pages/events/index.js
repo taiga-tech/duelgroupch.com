90060da98e838f660b04a4dc3182950290681cf6
taiga-tech <taiga.dev.n@gmail.com>
2021-10-14 03:48:22 +0900

    Update index.js

--- src/pages/events/index.js
+++ src/pages/events/index.js
@@ -1,5 +1,18 @@
+// mui components
+import Container from '@mui/material/Container'
+// local components
+import { MainLayout } from 'layouts/main'
+
 const Events = () => {
-  return <div>event</div>
+  const seo = { page: 'events' }
+
+  return (
+    <MainLayout seo={seo}>
+      <Container>
+        <h1>events</h1>
+      </Container>
+    </MainLayout>
+  )
 }
 
 export default Events
