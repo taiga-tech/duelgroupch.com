3f44a7fb2c4d0e17719270be0020d75d3b494596
taiga-tech <taiga.dev.n@gmail.com>
2021-10-26 17:29:58 +0900

    Update DGLogo DefaultProps

--- src/components/App/DGLogo.js
+++
@@ -1,7 +0,0 @@
-// next components
-import Image from 'next/image'
-
-export const DGLogo = ({ width, height }) => {
-  const logo = '/images/logo.png'
-  return <Image src={logo} width={width} height={height} alt="DUELGROUP" />
-}
