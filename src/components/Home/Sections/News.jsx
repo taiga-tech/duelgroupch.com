585c60fd53660aac1241f41b48138a23d0df1dd4
taiga-tech <taiga.dev.n@gmail.com>
2021-11-10 20:44:10 +0900

    Update News.jsx

--- src/components/Home/Sections/News.jsx
+++ src/components/Home/Sections/News.jsx
@@ -1,45 +1,32 @@
 // react modules
 import React, { useState, useEffect } from 'react'
-// next component
-import Script from 'next/script'
 // mui components
 import Container from '@mui/material/Container'
 import Box from '@mui/material/Box'
 import Grid from '@mui/material/Grid'
-import MuiLink from '@mui/material/Link'
 import Skeleton from '@mui/material/Skeleton'
 import Typography from '@mui/material/Typography'
-import Stack from '@mui/material/Stack'
+// other modules
+import loadable from '@loadable/component'
 // local components
 import { NewsItem } from 'components/Home/NewsItem'
-// local constants
-import { getNews } from 'constants/news'
+const TwitterWidgets = loadable(() => import('components/Home/Widgets/Twitter'))
 
-export const NewsSection = () => {
-  const [news, setNews] = useState([])
-
-  const fetchNews = async () => {
-    const data = await getNews()
-    setNews(data.slice(0, 5))
-  }
-
+export const NewsSection = ({ news, newsLength }) => {
+  const [loading, setLoading] = useState(true)
   useEffect(() => {
-    fetchNews()
+    setTimeout(() => setLoading(false), 2000)
   }, [])
-
-  const loading = news.length === 0
 
   return (
     <Container>
-      {/*  */}
-
       <Box sx={{ py: 3 }}>
-        {/*  */}
         <Grid
           container
           columnSpacing={2}
           rowSpacing={{ xs: 6, ms: 0 }}
           columns={12}
+          sx={{ minHeight: '640px' }}
         >
           <Grid
             item
@@ -48,13 +35,13 @@ export const NewsSection = () => {
             sx={{ display: 'flex', alignItems: 'center' }}
           >
             <Box sx={{ flexGrow: 1 }}>
-              <Typography variant="h4" gutterBottom>
+              <Typography variant="h4" component="h2" gutterBottom>
                 News
               </Typography>
 
-              <Grid container spacing={2}>
+              <Grid container spacing={2} sx={{ pl: 2 }}>
                 {loading
-                  ? Array.from(Array(5)).map((_, i) => (
+                  ? Array.from(Array(newsLength)).map((_, i) => (
                       <React.Fragment key={i}>
                         <Grid item xs={3} sm={2}>
                           <Typography variant="body2">
@@ -72,31 +59,17 @@ export const NewsSection = () => {
                       </React.Fragment>
                     ))
                   : news.map((item, index) => (
-                      <NewsItem item={item} key={index} />
+                      <NewsItem
+                        item={item.properties.Published.checkbox && item}
+                        key={index}
+                      />
                     ))}
               </Grid>
             </Box>
           </Grid>
-          {/*  */}
           <Grid item xs={12} md={5}>
             <Box sx={{ display: 'flex', justifyContent: 'center' }}>
-              <Box>
-                <MuiLink
-                  className="twitter-timeline"
-                  data-width="360"
-                  data-height="640"
-                  data-theme="dark"
-                  href="https://twitter.com/DuelGroup?ref_src=twsrc%5Etfw"
-                >
-                  Tweets by DuelGroup
-                </MuiLink>
-              </Box>
-              <Script
-                async
-                src="https://platform.twitter.com/widgets.js"
-                charSet="utf-8"
-                strategy="lazyOnload"
-              />
+              <TwitterWidgets />
             </Box>
           </Grid>
         </Grid>
