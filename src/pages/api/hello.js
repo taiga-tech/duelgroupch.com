21e8d3df582d8123dc35a47687a0029ad5718433
taiga-tech <taiga.dev.n@gmail.com>
2021-12-02 16:24:17 +0900

    Delete hello.js

--- src/pages/api/hello.js
+++
@@ -1,7 +0,0 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-
-const handler = async (req, res) => {
-  res.status(200).json({ name: 'taiga-tech' })
-}
-
-export default handler
