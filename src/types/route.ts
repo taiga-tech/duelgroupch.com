bf2505738855bfe1b5b62227a810ea741379f450
taiga-tech <taiga.dev.n@gmail.com>
2021-12-12 22:54:28 +0900

    Update route.ts

--- src/types/route.ts
+++ src/types/route.ts
@@ -1,9 +1,15 @@
-export interface RouteType {
-  to: string
+import type { OverridableComponent } from '@mui/types'
+import type { SvgIconTypeMap } from '@mui/material'
+
+export interface RouteObj {
   title: string
+  to: string
+  icon?: OverridableComponent<SvgIconTypeMap<{}, 'svg'>> & {
+    muiName: string
+  }
+  [key: string]: any
 }
 
-export type Route = {
-  home: RouteType
-  about: RouteType
+export type RouteType = {
+  [key: string]: RouteObj
 }
