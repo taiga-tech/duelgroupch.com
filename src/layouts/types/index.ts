01e3b4000eb3b080c34aa5ab1eb8d51070455a2f
taiga-tech <taiga.dev.n@gmail.com>
2021-12-19 06:32:23 +0900

    Update index.ts

--- src/layouts/types/index.ts
+++ src/layouts/types/index.ts
@@ -1,6 +1,7 @@
-import type { Seo } from 'types/next-seo'
+import type { Seo } from 'layouts/types/next-seo'
 
 export type LayoutProps = {
-  seo: Seo
-  children: React.ReactNode
+  seo?: Seo
+  headerPosition?: 'absolute' | 'fixed' | 'relative' | 'static' | 'sticky'
+  children: any
 }
