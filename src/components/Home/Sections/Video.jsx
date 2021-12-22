8a3b3d7fffdd76bba4aa9f81ca7eaae75cd5bd18
taiga-tech <taiga.dev.n@gmail.com>
2021-12-23 04:30:23 +0900

    Update Video.jsx add more button

--- src/components/Home/Sections/Video.jsx
+++ src/components/Home/Sections/Video.jsx
@@ -4,12 +4,16 @@ import { useState, useEffect } from 'react'
 import Container from '@mui/material/Container'
 import Box from '@mui/material/Box'
 import Grid from '@mui/material/Grid'
+import Button from '@mui/material/Button'
+// mui icons
+import YouTubeIcon from '@mui/icons-material/YouTube'
 // local components
 import { VideoCardWrapper } from 'components/Home/VideoCardWrapper'
 import { VideoCard } from 'components/Home/VideoCard'
 import { ErrorAlert } from 'components/App/ErrorAlert'
 
 export const VideoSection = ({ video }) => {
+  const channelUrl = 'https://www.youtube.com/channel/UCE010VqCfjLp7zckSBbFyfw/'
   const [loading, setLoading] = useState(true)
   useEffect(() => {
     setTimeout(() => setLoading(false), 2000)
@@ -32,6 +36,28 @@ export const VideoSection = ({ video }) => {
                 </Grid>
               ))}
         </VideoCardWrapper>
+        <Box
+          sx={{
+            display: 'flex',
+            justifyContent: 'center',
+            p: 2,
+          }}
+        >
+          {video.items.length >= 16 && (
+            <Button
+              variant="outlined"
+              color="inherit"
+              href={channelUrl}
+              target="_blank"
+              rel="noopener noreferrer"
+              size="large"
+              endIcon={<YouTubeIcon color="error" />}
+              sx={{ px: 12 }}
+            >
+              もっと見る
+            </Button>
+          )}
+        </Box>
       </Box>
     </Container>
   )
