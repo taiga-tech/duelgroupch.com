6a31f2293117e392879e452279878376c37a033e
taiga-tech <taiga.dev.n@gmail.com>
2021-12-24 09:53:45 +0900

    ts header

--- src/components/App/Socials.tsx
+++ src/components/App/Socials.tsx
@@ -6,23 +6,21 @@ import IconButton from '@mui/material/IconButton'
 // local constants
 import { SOCIALS } from 'constants/social'
 
-export const Socials = (): JSX.Element => {
-  return (
-    <Stack spacing={2} direction="row">
-      {SOCIALS.map((sns: SocialObj, i: number) => (
-        <IconButton
-          sx={sns.sx}
-          target="_blank"
-          rel="noopener noreferrer"
-          href={sns.href}
-          aria-label={sns.title}
-          title={sns.title}
-          size="small"
-          key={i}
-        >
-          {sns.icon}
-        </IconButton>
-      ))}
-    </Stack>
-  )
-}
+export const Socials = (): JSX.Element => (
+  <Stack spacing={2} direction="row">
+    {SOCIALS.map((sns: SocialObj, i: number) => (
+      <IconButton
+        sx={sns.sx}
+        target="_blank"
+        rel="noopener noreferrer"
+        href={sns.href}
+        aria-label={sns.title}
+        title={sns.title}
+        size="small"
+        key={i}
+      >
+        {sns.icon}
+      </IconButton>
+    ))}
+  </Stack>
+)
