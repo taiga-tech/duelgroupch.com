a58cbf5001a6cd28d0671b4cebdc37b120c2a9cd
taiga-tech <taiga.dev.n@gmail.com>
2021-12-24 14:57:02 +0900

    Create index.ts

---
+++ src/components/notion/index.ts
@@ -0,0 +1,9 @@
+import Heading from 'components/notion/heading'
+import Callout from 'components/notion/callout'
+
+const notionComponents = {
+  Heading: Heading,
+  Callout: Callout,
+}
+
+export default notionComponents
