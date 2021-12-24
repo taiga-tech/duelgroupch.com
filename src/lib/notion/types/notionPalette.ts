2d260b6ea5ef1ea24192b76a9229060425b31cc3
taiga-tech <taiga.dev.n@gmail.com>
2021-12-24 15:39:03 +0900

    Create notionPalette.ts

---
+++ src/lib/notion/types/notionPalette.ts
@@ -0,0 +1,26 @@
+// TODO: Add Notion Palette to mui Theme
+
+export interface notionPalette {
+  block_color?:
+    | 'default'
+    | 'gray'
+    | 'brown'
+    | 'red'
+    | 'orange'
+    | 'yellow'
+    | 'green'
+    | 'blue'
+    | 'purple'
+    | 'pink'
+    | 'background_default'
+    | 'background_gray'
+    | 'background_brown'
+    | 'background_red'
+    | 'background_orange'
+    | 'background_yellow'
+    | 'background_green'
+    | 'background_blue'
+    | 'background_purple'
+    | 'background_pink'
+    | undefined
+}
