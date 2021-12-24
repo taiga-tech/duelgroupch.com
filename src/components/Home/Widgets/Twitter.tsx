aaeff3c74a87a71a007c5ac121954be1f1d568e0
taiga-tech <taiga.dev.n@gmail.com>
2021-12-24 13:34:15 +0900

    ts twitterWidgets

--- src/components/Home/Widgets/Twitter.tsx
+++ src/components/Home/Widgets/Twitter.tsx
@@ -1,21 +1,31 @@
 // react
-import { useRef, useEffect } from 'react'
+import { useEffect } from 'react'
 // next components
 import Script from 'next/script'
 // mui components
 import Box from '@mui/material/Box'
 import MuiLink from '@mui/material/Link'
-import { useTheme } from '@mui/styles'
+// mui lib
+import { useContext } from 'react'
+import { ThemeContext } from 'components/MyThemeProvider'
+
+declare global {
+  interface Window {
+    twttr: any
+  }
+}
 
 const TwitterWidgets = () => {
-  const theme = useTheme()
+  const [theme, lsTheme, handleLightMode, handleSystemTheme, handleDarkMode] =
+    useContext(ThemeContext)
+
   useEffect(() => {
     const twitterSrc = 'https://platform.twitter.com/widgets.js'
 
-    if (window?.twttr?.widgets) {
-      window.twttr.widgets.load()
+    if (window?.twttr.widgets) {
+      window?.twttr.widgets.load()
     } else if (!document.querySelector(`script[src="${twitterSrc}"]`)) {
-      const script = document.createElement(script)
+      const script = document.createElement('script')
       script.async = true
       script.src = twitterSrc
       document.querySelector('body').appendChild(script)
@@ -28,7 +38,7 @@ const TwitterWidgets = () => {
         className="twitter-timeline"
         data-width="360"
         data-height="640"
-        data-theme={theme.palette.mode}
+        data-theme={theme}
         href="https://twitter.com/DuelGroup?ref_src=twsrc%5Etfw"
       >
         Tweets by DuelGroup
