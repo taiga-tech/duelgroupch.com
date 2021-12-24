294ce4ef0dba505ebca706f942264a2a0514f206
taiga-tech <taiga.dev.n@gmail.com>
2021-12-24 13:46:14 +0900

    Update ext-link.tsx

--- src/components/App/Dynamic/ext-link.tsx
+++ src/components/App/Dynamic/ext-link.tsx
@@ -2,7 +2,11 @@
 import MuiLink from '@mui/material/Link'
 
 const ExtLink = (props) => (
-  <MuiLink {...props} rel="noopener" target={props.target || '_blank'} />
+  <MuiLink
+    {...props}
+    rel="noopener noreferrer"
+    target={props.target || '_blank'}
+  />
 )
 
 export default ExtLink
