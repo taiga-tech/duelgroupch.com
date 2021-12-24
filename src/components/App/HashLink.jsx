4b3982bf38c6eaf8980314c0d1c4e11d97634b5d
taiga-tech <taiga.dev.n@gmail.com>
2021-12-24 09:51:35 +0900

    ts hashlink

--- src/components/App/HashLink.jsx
+++
@@ -1,9 +0,0 @@
-import { styled } from '@mui/material/styles'
-import AnchorLink from 'react-anchor-link-smooth-scroll'
-
-export const HashLink = styled((props) => <AnchorLink {...props} />)(
-  ({ theme }) => ({
-    color: theme.palette.mode === 'dark' ? '#fff' : '#000',
-    textDecoration: 'none',
-  })
-)
