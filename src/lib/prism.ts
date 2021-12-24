c3269e93f1ef000137065444946466045561c4e9
taiga-tech <taiga.dev.n@gmail.com>
2021-12-24 14:16:53 +0900

    Create prism.ts

---
+++ src/lib/prism.ts
@@ -0,0 +1,18 @@
+import Prism from 'prismjs'
+
+// Include the theme plugin: (optional)
+import 'prism-themes/themes/prism-coldark-dark.css'
+
+// Include the languages plugin: (optional)
+import 'prismjs/components/prism-markup-templating'
+import 'prismjs/components/prism-bash'
+import 'prismjs/components/prism-scss'
+import 'prismjs/components/prism-javascript'
+import 'prismjs/components/prism-ruby'
+import 'prismjs/components/prism-php'
+import 'prismjs/components/prism-docker'
+import 'prismjs/components/prism-yaml'
+import 'prismjs/components/prism-json'
+import 'prismjs/components/prism-git'
+
+export default Prism
