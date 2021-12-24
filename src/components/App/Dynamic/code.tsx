94576da2a7e5cb3727db64f7717bf1915b7f1aa5
taiga-tech <taiga.dev.n@gmail.com>
2021-12-24 13:45:57 +0900

    Update code.tsx

--- src/components/App/Dynamic/code.tsx
+++ src/components/App/Dynamic/code.tsx
@@ -1,24 +1,37 @@
+// prism
+import Prism from 'lib/prism'
 // mui components
 import Box from '@mui/material/Box'
 
-const Code = ({ children, language }: { children: any; language: any }) => (
-  <Box component="pre" sx={{ tabSize: 4 }}>
-    <Box
-      component="code"
-      sx={{
-        overflow: 'auto',
-        display: 'block',
-        padding: '0.8rem',
-        lineHeight: 1.5,
-        background: '#111b27',
-        fontSize: '0.75rem',
-        borderRadius: '0.5rem',
-        maxHeight: 500,
-      }}
-    >
-      {children}
+const Code = ({ children, language }: { children: any; language: string }) => {
+  return (
+    <Box component="pre">
+      <Box
+        component="code"
+        sx={{
+          overflow: 'auto',
+          display: 'block',
+          p: 3,
+          lineHeight: 1.5,
+          background: '#111b27',
+          fontSize: 14,
+          borderRadius: '0.5rem',
+          maxHeight: 500,
+          tabSize: 2,
+          fontFamily:
+            'Consolas, Menlo, Monaco, "Andale Mono", "Ubuntu Mono", monospace',
+          // fontFamily: '"Lucida Console", Courier, monospace',
+        }}
+        dangerouslySetInnerHTML={{
+          __html: Prism.highlight(
+            children,
+            Prism.languages[language.toLowerCase()] ||
+              Prism.languages.javascript
+          ),
+        }}
+      />
     </Box>
-  </Box>
-)
+  )
+}
 
 export default Code
