c21403173bd6da00694477153e90ea7584e4cae9
taiga-tech <taiga.dev.n@gmail.com>
2022-01-02 23:23:00 +0900

    Update callout.tsx

--- src/components/notion/callout.tsx
+++ src/components/notion/callout.tsx
@@ -6,22 +6,49 @@ import Box from '@mui/material/Box'
 import { textBlock } from 'lib/notion/renderers'
 
 const Callout = ({ id, value, properties }) => {
+  const isLink = /^(https?|ftp)(:\/\/[\w\/:%#\$&\?\(\)~\.=\+\-]+)/
+  let linkIcon: string
+  let planeIcon: string
+  if (value.format?.page_icon.match(isLink)) {
+    linkIcon = value.format?.page_icon
+  } else {
+    planeIcon = value.format?.page_icon
+  }
+
   return (
-    <Box
-      sx={{
-        ml: 1,
-        py: 2,
-        pr: 2,
-        pl: '12px',
-        display: 'flex',
-        width: '100%',
-        borderRadius: '3px',
-        background: 'rgba(60, 65, 68, 0.6)',
-      }}
-      key={id}
-    >
-      {value.format?.page_icon && <Box>{value.format?.page_icon}</Box>}
-      <Box sx={{ ml: 1 }}>{textBlock(properties.title, true, id)}</Box>
+    <Box sx={{ my: 1 }}>
+      <Box
+        key={id}
+        sx={{
+          py: 2,
+          pr: 2,
+          pl: '12px',
+          width: '100%',
+          display: 'flex',
+          borderRadius: '3px',
+          background: '#202020',
+        }}
+      >
+        {planeIcon && <Box>{planeIcon}</Box>}
+        {linkIcon && (
+          /* eslint @next/next/no-img-element: off */
+          <img
+            src={`/api/asset?assetUrl=${encodeURIComponent(
+              linkIcon as any
+            )}&blockId=${id}`}
+            alt="callout-icon"
+            style={{
+              width: 21.312,
+              height: 21.312,
+              borderRadius: 3,
+              display: 'block',
+              objectFit: 'cover',
+              transition: 'opacity 100ms ease-out 0s',
+            }}
+          />
+        )}
+        <Box sx={{ ml: 1 }}>{textBlock(properties.title, true, id)}</Box>
+      </Box>
     </Box>
   )
 }
