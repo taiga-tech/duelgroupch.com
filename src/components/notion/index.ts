9b476cc22794ca4b625835f44d235a1372d58cc0
taiga-tech <taiga.dev.n@gmail.com>
2022-01-02 23:27:16 +0900

    Update index.ts

--- src/components/notion/index.ts
+++ src/components/notion/index.ts
@@ -1,9 +1,13 @@
 import Heading from 'components/notion/heading'
 import Callout from 'components/notion/callout'
+import ArticleSkelton from 'components/notion/skelton'
+import Preview from 'components/notion/preview'
 
 const notionComponents = {
-  Heading: Heading,
-  Callout: Callout,
+  Heading,
+  Callout,
+  ArticleSkelton,
+  Preview,
 }
 
 export default notionComponents
