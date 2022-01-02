334808134f7c9c861c941a1b8e17e2abef1736c2
taiga-tech <taiga.dev.n@gmail.com>
2022-01-02 13:27:16 +0900

    Update index.tsx

--- src/components/App/Dynamic/index.tsx
+++ src/components/App/Dynamic/index.tsx
@@ -2,6 +2,8 @@
 import dynamic from 'next/dynamic'
 // local components
 import ExtLink from 'components/App/Dynamic/ext-link'
+import { Typography } from '@mui/material'
+// import Typography from '@mui/material/Typography'
 
 const components = {
   // default tags
@@ -9,9 +11,10 @@ const components = {
   ul: 'ul',
   li: 'li',
   p: 'p',
+  // p: Typography,
   blockquote: 'blockquote',
   a: ExtLink,
-
+  P: Typography,
   Code: dynamic(() => import('components/App/Dynamic/code')),
   Counter: dynamic(() => import('components/App/Dynamic/counter')),
   Equation: dynamic(() => import('components/App/Dynamic/equation')),
