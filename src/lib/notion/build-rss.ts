f0ef3db5acbf042a6e54d750749fbfd62cb04631
taiga-tech <taiga.dev.n@gmail.com>
2022-01-08 00:41:16 +0900

    Update build-rss.ts

--- src/lib/notion/build-rss.ts
+++ src/lib/notion/build-rss.ts
@@ -6,8 +6,8 @@ import { renderToStaticMarkup } from 'react-dom/server'
 import { loadEnvConfig } from '@next/env'
 // local lib
 import { writeFile } from 'lib/fsHelper'
-import { postIsPublished, getPageLink } from 'lib/notion/notion-helpers'
-import { textBlock } from 'lib/notion/renderers'
+import { postIsPublished, getPageLink } from 'lib/notion/notionHelpers'
+import { pBlock } from 'lib/notion/renderers'
 import getPageIndex from 'lib/notion/getPageIndex'
 import getNotionUsers from 'lib/notion/getNotionUsers'
 import serverConstants from 'constants/notion/server-constants'
@@ -19,11 +19,11 @@ process.env.USE_CACHE = 'true'
 // constants
 const NOW = new Date().toJSON()
 
-function mapToAuthor(author) {
+const mapToAuthor = (author) => {
   return `<author><name>${author.full_name}</name></author>`
 }
 
-function decode(string) {
+const decode = (string) => {
   return string
     .replace(/&/g, '&amp;')
     .replace(/</g, '&lt;')
@@ -32,50 +32,50 @@ function decode(string) {
     .replace(/'/g, '&apos;')
 }
 
-function mapToEntry(post) {
+const mapToEntry = (post) => {
   return `
-    <entry>
-      <id>${post.link}</id>
-      <title>${decode(post.title)}</title>
-      <link href="${post.link}"/>
-      <updated>${new Date(post.date).toJSON()}</updated>
-      <content type="xhtml">
-        <div xmlns="http://www.w3.org/1999/xhtml">
-          ${renderToStaticMarkup(
-            post.preview
-              ? (post.preview || []).map((block, idx) =>
-                  textBlock(block, false, post.title + idx)
-                )
-              : post.content
-          )}
-          <p class="more">
-            <a href="${post.link}">Read more</a>
-          </p>
-        </div>
-      </content>
-      ${(post.authors || []).map(mapToAuthor).join('\n      ')}
-    </entry>`
+  <entry>
+    <id>${post.link}</id>
+    <title>${decode(post.title)}</title>
+    <link href="${post.link}" />
+    <updated>${new Date(post.date).toJSON()}</updated>
+    <content type="xhtml">
+      <div xmlns="http://www.w3.org/1999/xhtml">
+        ${renderToStaticMarkup(
+          post.preview
+            ? (post.preview || []).map((block, idx) =>
+                pBlock(block, false, post.title + idx)
+              )
+            : post.content
+        )}
+        <p class="more">
+          <a href="${post.link}">Read more</a>
+        </p>
+      </div>
+    </content>
+    ${(post.authors || []).map(mapToAuthor).join('\n      ')}
+  </entry>`
 }
 
-function concat(total, item) {
+const concat = (total, item) => {
   return total + item
 }
 
-function createRSS(blogPosts = []) {
-  const postsString = blogPosts.map(mapToEntry).reduce(concat, '')
+const createRSS = (pagePosts = []) => {
+  const postsString = pagePosts.map(mapToEntry).reduce(concat, '')
 
   return `<?xml version="1.0" encoding="utf-8"?>
-          <feed xmlns="http://www.w3.org/2005/Atom">
-            <title>My Notion Page</title>
-            <subtitle>Pages</subtitle>
-            <link href="/atom" rel="self" type="application/rss+xml"/>
-            <link href="/" />
-            <updated>${NOW}</updated>
-            <id>My Notion Page</id>${postsString}
-          </feed>`
+<feed xmlns="http://www.w3.org/2005/Atom">
+  <title>My Notion Page</title>
+  <subtitle>Pages</subtitle>
+  <link href="/atom" rel="self" type="application/rss+xml" />
+  <link href="/" />
+  <updated>${NOW}</updated>
+  <id>My Notion Page</id>${postsString}
+</feed>`
 }
 
-async function main() {
+const main = async () => {
   await loadEnvConfig(process.cwd())
   serverConstants.NOTION_TOKEN = process.env.NOTION_TOKEN
   serverConstants.PAGE_INDEX_ID = serverConstants.normalizeId(
@@ -85,7 +85,7 @@ async function main() {
   const postsTable = await getPageIndex(true)
   const neededAuthors = new Set<string>()
 
-  const blogPosts = Object.keys(postsTable)
+  const pagePosts = Object.keys(postsTable)
     .map((slug) => {
       const post = postsTable[slug]
       if (!postIsPublished(post)) return
@@ -101,7 +101,7 @@ async function main() {
 
   const { users } = await getNotionUsers([...neededAuthors])
 
-  blogPosts.forEach((post) => {
+  pagePosts.forEach((post) => {
     post.authors = post.authors.map((id) => users[id])
     post.link = getPageLink(post.Params, post.Slug)
     post.title = post.Page
@@ -109,7 +109,7 @@ async function main() {
   })
 
   const outputPath = 'public/atom'
-  await writeFile(resolve(outputPath), createRSS(blogPosts))
+  await writeFile(resolve(outputPath), createRSS(pagePosts))
   console.log(`Atom feed file generated at \`${outputPath}\``)
 }
 
